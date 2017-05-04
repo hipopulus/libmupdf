1e335930d64f7e19c13536847b18431fca97dba2
chengwen.yang 杨程文 <chengwen.yang@qiyuesuo.com>
2017-05-04 14:06:00 +0800

    Update pom file

--- src/main/java/com/artifex/mupdf/fitz/TryLaterException.java
+++ src/main/java/com/artifex/mupdf/fitz/TryLaterException.java
@@ -2,6 +2,8 @@ package com.artifex.mupdf.fitz;
 
 public class TryLaterException extends RuntimeException
 {
+	private static final long serialVersionUID = 1L;
+
 	TryLaterException(String message) {
 		super(message);
 	}
